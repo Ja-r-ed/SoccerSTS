7a965a55117da872e9a030f99bd6540e4f71477f
Jared Jin <jared.jj.jin@gmail.com>
2025-07-27 11:23:56 +1000

    before/during video sunday

--- motors/motors.ino
+++ motors/motors.ino
@@ -1,28 +1,51 @@
 #include <IRremote.hpp>
 #include <Arduino.h>
 #define DECODE_NEC
-#include "PinDefinitionsAndMore.h"
 
-int M1a = 7;
-int M1b = 6;  
-int M2a = 4;
-int M2b = 5;  
-int M3a = 10;
-int M3b = 11;  
-int M4a = 2;
-int M4b = 3;  
+int M1a = 5;
+int M1b = 4;
+int M2a = 3;
+int M2b = 2;
+int M3a = 6;
+int M3b = 7;
+int M4a = 12;
+int M4b = 11;
 
-int Da = 11;
-int Db = 12;
-int DPWM = 10;  
+int d1 = 22;
+int d2 = 23;
+int dena = 13;
 
-int GlobalPWM = 40;
-//   Front Left  (M1)
-//   Front Right (M2)
-//   Back Left   (M3)
-//   Back Right  (M4)
+/*
+Front Left  (M1)
+Front Right (M2)
+Back Left   (M3)
+Back Right  (M4)
 
-//
+m4a = YELLOW, 11 left side of robot
+m4B = BLACK, 12
+m2a = ORANGE, 2
+m2b = BROWN, 3 
+m1a = YELLOW, 4 right side of robot
+m1b = ORANGE, 5
+m3a = GREY, 6
+m3b = WHITE, 7 
+
+d1 = RED, 22
+d2 = BLACK, 23
+dena = BROWN, 13
+*/
+
+int RotatePWM = 50;
+int StraightPWM = 60;
+
+const unsigned long TIMEOUT_MS = 200;  // 3 seconds timeout
+unsigned long lastSignalTime = 0;
+
+int M1Target = 0;
+int M2Target = 0;
+int M3Target = 0;
+int M4Target = 0;
+
 void setup() {
   IrReceiver.begin(A0, ENABLE_LED_FEEDBACK);
   pinMode(M1a, OUTPUT);
@@ -33,67 +56,92 @@ void setup() {
   pinMode(M3b, OUTPUT);
   pinMode(M4a, OUTPUT);
   pinMode(M4b, OUTPUT);
+  pinMode(d1, OUTPUT);
+  pinMode(d2, OUTPUT);
+  pinMode(dena, OUTPUT);
+  delay(100);
 }
 
 void loop() {
+  // if (IrReceiver.decode()) {
+  //   //if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
+  //   //  IrReceiver.printIRResultRawFormatted(&Serial, true);
+  //   //  IrReceiver.resume();  // Do it here, to preserve raw data for printing with printIRResultRawFormatted()
+  //   //} else {
+  //     IrReceiver.resume();  // Early enable receiving of the next IR frame
+  //     lastSignalTime = millis();
+  //     IrReceiver.printIRResultShort(&Serial);
+  //     IrReceiver.printIRSendUsage(&Serial);
+  //   //}
+  // }
 
-  if (IrReceiver.decode()) {
-    if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
-      IrReceiver.printIRResultRawFormatted(&Serial, true);
-      IrReceiver.resume();  // Do it here, to preserve raw data for printing with printIRResultRawFormatted()
-    } else {
-      IrReceiver.resume();  // Early enable receiving of the next IR frame
-      IrReceiver.printIRResultShort(&Serial);
-      IrReceiver.printIRSendUsage(&Serial);
-    }
-    Serial.println();
-    if (IrReceiver.decodedIRData.command == 0x18) {  // forwards
-      drive(0, 0.2, 0);
-    } else if (IrReceiver.decodedIRData.command == 0x52) {  // backwards
-      drive(180, 0.2, 0);
-    } else if (IrReceiver.decodedIRData.command == 0x5A) {  // right
-      drive(90, 0.2, 0);
-    } else if (IrReceiver.decodedIRData.command == 0x8) {  // left
-      drive(270, 0.2, 0);
-    } else if (IrReceiver.decodedIRData.command == 0x45) {  // turn left
-      digitalWrite(M1a, LOW);
-      analogWrite(M1b, GlobalPWM);
+  // if (millis() - lastSignalTime > TIMEOUT_MS) {
+  //   // No IR signal received recently — stop main code
+  //   digitalWrite(M1a, LOW);
+  //   digitalWrite(M1b, LOW);
+  //   digitalWrite(M2a, LOW);
+  //   digitalWrite(M2b, LOW);
+  //   digitalWrite(M3a, LOW);
+  //   digitalWrite(M3b, LOW);
+  //   digitalWrite(M4a, LOW);
+  //   digitalWrite(M4b, LOW);
 
-      digitalWrite(M2a, LOW);
-      analogWrite(M2b, GlobalPWM);
+  //   digitalWrite(d1, LOW);
+  //   digitalWrite(d2, LOW);
+  //   digitalWrite(dena, LOW);
+  // } else {
+  //   // IR signal received recently — run main code
+  //   if (IrReceiver.decodedIRData.command == 0x18) {  // forwards
+  //     drive(0, 0.2, 0);
+  //   } else if (IrReceiver.decodedIRData.command == 0x52) {  // backwards
+  //     drive(180, 0.2, 0);
+  //   } else if (IrReceiver.decodedIRData.command == 0x5A) {  // right
+  //     drive(90, 0.2, 0);
+  //   } else if (IrReceiver.decodedIRData.command == 0x8) {  // left
+  //     drive(270, 0.2, 0);
+  //   } else if (IrReceiver.decodedIRData.command == 0x45) {  // rotate left
+  //     digitalWrite(M1a, LOW);
+  //     analogWrite(M1b, RotatePWM);
 
-      digitalWrite(M3a, LOW);
-      analogWrite(M3b, GlobalPWM);
+  //     digitalWrite(M2a, LOW);
+  //     analogWrite(M2b, RotatePWM);
 
-      digitalWrite(M4a, LOW);
-      analogWrite(M4b, GlobalPWM);
-    } else if (IrReceiver.decodedIRData.command == 0x46) {  // dribble
-      digitalWrite(Da, HIGH);
-      digitalWrite(Db, LOW);
-      analogWrite(DPWM, 40);
-    } else if (IrReceiver.decodedIRData.command == 0x47) {  // turn right
-      digitalWrite(M1b, LOW);
-      analogWrite(M1a, GlobalPWM);
+  //     digitalWrite(M3a, LOW);
+  //     analogWrite(M3b, RotatePWM);
 
-      digitalWrite(M2b, LOW);
-      analogWrite(M2a, GlobalPWM);
+  //     digitalWrite(M4a, LOW);
+  //     analogWrite(M4b, RotatePWM);
+  //   } else if (IrReceiver.decodedIRData.command == 0x46) { // dribble
+  //   digitalWrite(d1, LOW);
+  //   digitalWrite(d2, HIGH);
+  //   analogWrite(dena, 90);
+  //   } else if (IrReceiver.decodedIRData.command == 0x47) {  // rotate right
+  //     digitalWrite(M1b, LOW);
+  //     analogWrite(M1a, RotatePWM);
 
-      digitalWrite(M3b, LOW);
-      analogWrite(M3a, GlobalPWM);
+  //     digitalWrite(M2b, LOW);
+  //     analogWrite(M2a, RotatePWM);
 
-      digitalWrite(M4b, LOW);
-      analogWrite(M4a, GlobalPWM);
-    }
-    delay(100);
-    digitalWrite(M1a, LOW);
-    digitalWrite(M1b, LOW);
-    digitalWrite(M2a, LOW);
-    digitalWrite(M2b, LOW);
-    digitalWrite(M3a, LOW);
-    digitalWrite(M3b, LOW);
-    digitalWrite(M4a, LOW);
-    digitalWrite(M4b, LOW);
-  }
+  //     digitalWrite(M3b, LOW);
+  //     analogWrite(M3a, RotatePWM);
+
+  //     digitalWrite(M4b, LOW);
+  //     analogWrite(M4a, RotatePWM);
+  //   }
+  // }
+
+  // drive(0, 0.2, 0);
+  // delay(1000);
+
+  // drive(90, 0.2, 0);
+  // delay(1000);
+
+  // drive(180, 0.2, 0);
+  // delay(1000);
+
+  // drive(270, 0.2, 0);
+  // delay(1000);
+  drive(0, 0.4, 0);
 }
 
 void MotorTest() {
@@ -111,113 +159,9 @@ void MotorTest() {
 }
 
 void dribble() {
-  digitalWrite(Da, HIGH);
-  digitalWrite(Db, LOW);
-  analogWrite(DPWM, 20);
-}
-
-void SetSpeedPID(int Motor, int PWM) {
-  static int PWM1Value = 0;
-  static int PWM2Value = 0;
-  static int PWM3Value = 0;
-  static int PWM4Value = 0;
-
-  if (Motor == 1) {
-    if (PWM > 0) {
-      analogWrite(M1a, pidController(PWM - PWM1Value));
-      digitalWrite(M1b, LOW);
-    }
-    if (PWM < 0) {
-      digitalWrite(M1a, LOW);
-      analogWrite(M1b, pidController(PWM - PWM1Value));
-    }
-    if (PWM = 0) {
-      digitalWrite(M1a, LOW);
-      digitalWrite(M1b, LOW);
-    }
-    int PWM1Value = pidController(PWM - PWM1Value);
-  }
-
-  if (Motor == 2) {
-    if (PWM > 0) {
-      analogWrite(M2a, pidController(PWM - PWM2Value));
-      digitalWrite(M2b, LOW);
-    }
-    if (PWM < 0) {
-      digitalWrite(M2a, LOW);
-      analogWrite(M2b, pidController(PWM - PWM2Value));
-    }
-    if (PWM = 0) {
-      digitalWrite(M2a, LOW);
-      digitalWrite(M2b, LOW);
-    }
-    int PWM2Value = pidController(PWM - PWM2Value);
-  }
-
-  if (Motor == 3) {
-    if (PWM > 0) {
-      analogWrite(M3a, pidController(PWM - PWM3Value));
-      digitalWrite(M3b, LOW);
-    }
-    if (PWM < 0) {
-      digitalWrite(M3a, LOW);
-      analogWrite(M3b, pidController(PWM - PWM3Value));
-    }
-    if (PWM = 0) {
-      digitalWrite(M3a, LOW);
-      digitalWrite(M3b, LOW);
-    }
-    int PWM3Value = pidController(PWM - PWM3Value);
-  }
-
-  if (Motor == 4) {
-    if (PWM > 0) {
-      analogWrite(M4a, pidController(PWM - PWM4Value));
-      digitalWrite(M4b, LOW);
-    }
-    if (PWM < 0) {
-      digitalWrite(M4a, LOW);
-      analogWrite(M4b, pidController(PWM - PWM4Value));
-    }
-    if (PWM = 0) {
-      digitalWrite(M4a, LOW);
-      digitalWrite(M4b, LOW);
-    }
-    int PWM4Value = pidController(PWM - PWM4Value);
-  }
-}
-
-float pidController(float error) {
-  float kp = 0.1;
-  float ki = 0;
-  float kd = 0;
-  static float previousError = 0.0;
-  static float integral = 0.0;
-
-  // Calculate proportional term
-  float proportional = kp * error;
-
-  // Calculate integral term
-  integral += error;
-  float integralTerm = ki * integral;
-
-  // Calculate derivative term
-  float derivative = kd * (error - previousError);
-
-  // Update previous error
-  previousError = error;
-
-  // Calculate PID output
-  float pidOut = proportional + integralTerm + derivative;
-
-  // Clamp PID output to the range [-1, 1]
-  if (pidOut > 1) pidOut = 1;
-  if (pidOut < -1) pidOut = -1;
-
-  pidOut = pidOut * 255;
-  pidOut = (int)(pidOut);
-
-  return pidOut;
+  digitalWrite(d1, LOW);
+  digitalWrite(d2, HIGH);
+  analogWrite(dena, 70);
 }
 
 void drive(float direction_deg, float speed, float rotation) {
@@ -228,10 +172,10 @@ void drive(float direction_deg, float speed, float rotation) {
 
     // X-drive kinematics for 4 wheels: FL, FR, BR, BL
     float wheel_speeds[4];
-    wheel_speeds[0] = 255 * vx * sin(45 * 3.14159265 / 180.0) + vy * cos(45 * 3.14159265 / 180.0) + rotation;
-    wheel_speeds[1] = 255 * vx * sin(-45 * 3.14159265 / 180.0) + vy * cos(-45 * 3.14159265 / 180.0) + rotation;
-    wheel_speeds[2] = 255 * vx * sin(-135 * 3.14159265 / 180.0) + vy * cos(-135 * 3.14159265 / 180.0) + rotation;
-    wheel_speeds[3] = 255 * vx * sin(135 * 3.14159265 / 180.0) + vy * cos(135 * 3.14159265 / 180.0) + rotation;
+    wheel_speeds[0] = vx * sin(45 * 3.14159265 / 180.0) + vy * cos(45 * 3.14159265 / 180.0) + rotation;
+    wheel_speeds[1] = vx * sin(-45 * 3.14159265 / 180.0) + vy * cos(-45 * 3.14159265 / 180.0) + rotation;
+    wheel_speeds[2] = vx * sin(-135 * 3.14159265 / 180.0) + vy * cos(-135 * 3.14159265 / 180.0) + rotation;
+    wheel_speeds[3] = vx * sin(135 * 3.14159265 / 180.0) + vy * cos(135 * 3.14159265 / 180.0) + rotation;
 
     // Normalize speeds if needed
     float max_speed = 0;
@@ -246,10 +190,10 @@ void drive(float direction_deg, float speed, float rotation) {
         }
     }
 
-    SetSpeed(1, wheel_speeds[0]);
-    SetSpeed(2, wheel_speeds[1]);
-    SetSpeed(3, wheel_speeds[3]);
-    SetSpeed(4, wheel_speeds[2]);
+    SetSpeed(1, wheel_speeds[0]*255);
+    SetSpeed(2, wheel_speeds[1]*255);
+    SetSpeed(3, wheel_speeds[3]*255);
+    SetSpeed(4, wheel_speeds[2]*255);
 }
 
 void SetSpeed(int Motor, int PWM) {
@@ -265,7 +209,7 @@ void SetSpeed(int Motor, int PWM) {
     }
     if (PWM < 0) {
       digitalWrite(M1a, LOW);
-      analogWrite(M1b, PWM);
+      analogWrite(M1b, -PWM);
     }
     if (PWM = 0) {
       digitalWrite(M1a, LOW);
@@ -281,7 +225,7 @@ void SetSpeed(int Motor, int PWM) {
     }
     if (PWM < 0) {
       digitalWrite(M2a, LOW);
-      analogWrite(M2b, PWM);
+      analogWrite(M2b, -PWM);
     }
     if (PWM = 0) {
       digitalWrite(M2a, LOW);
@@ -297,7 +241,7 @@ void SetSpeed(int Motor, int PWM) {
     }
     if (PWM < 0) {
       digitalWrite(M3a, LOW);
-      analogWrite(M3b, PWM);
+      analogWrite(M3b, -PWM);
     }
     if (PWM = 0) {
       digitalWrite(M3a, LOW);
@@ -313,7 +257,7 @@ void SetSpeed(int Motor, int PWM) {
     }
     if (PWM < 0) {
       digitalWrite(M4a, LOW);
-      analogWrite(M4b, PWM);
+      analogWrite(M4b, -PWM);
     }
     if (PWM = 0) {
       digitalWrite(M4a, LOW);
